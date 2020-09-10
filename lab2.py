085ac7bf64363d14bb261d30642eda6768672b35
unknown <cristiano_aguilar19@hotmail.com>
2020-09-09 23:27:59 -0700

    Signed-off-by: unknown <cristiano_aguilar19@hotmail.com>

--- lab2.py
+++ lab2.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Sep  3 10:26:37 2020
-
+@class:comp 469
 @author: Cristian Aguilar
+@Title: Lab2
 """
 import string
-import sys
+import queue
+
 
 class Tree():
     def __init__(self,root):
@@ -37,78 +39,71 @@ def childernFinder(str1, dictList):
 
 def findPath(start, end, node):
         curr = node
-        listResult = []
-        fringe.append(node.data)
+        result.append(node.data)
         for i in range(0, len(curr.children1)):
-            if(fringe[0] == start and fringe[len(fringe) - 1] == end):
-                listResult = fringe
-                return listResult
+            if(result[0] == start and result[len(result) - 1] == end):
+                return result
             else:
                 findPath(start, end, curr.children1[i])
                 
-        if(fringe[0] == start and fringe[len(fringe) - 1] == end):
-            listResult = fringe
-            return listResult
+        if(result[0] == start and result[len(result) - 1] == end):
+            return result
         else:
-            fringe.remove(node.data)
+            result.remove(node.data)
             
         
-"""use a list to push the entries in until the node hits a leaf node, then check 
- if the start and end of the list is correct if so return the list else continue to iterate through
- https://medium.com/basecs/breaking-down-breadth-first-search-cebe696709d9
-"""
 
-dictonary = ["try", "toy", "cop","cup","coy","fry","cry","bay","lay","boy","bow","fey"]
+dictonary = ["toy", "cop","try","coy","fry","cry","bay","lay","boy","bow","cup", "fey"]
 
 startState = "boy"
 GoalState = "cup"
-fringe = []
+result = []
+fringe = queue.Queue()
+fringe.put(startState)
 done = False
 treeRoot = Tree(Node(startState))
 dictonary.pop(dictonary.index(startState))
 currentNode = treeRoot.root
-
 currentNode.addNode1(childernFinder(startState, dictonary))
-#print(currentNode.children1[2].data)
+
+fringe.get()
+for i in range(0, len(currentNode.children1)):
+    fringe.put(currentNode.children1[i].data)
+                
 currLen = len(currentNode.children1)
-index = 0
-
-
 currentNode = currentNode.children1
-
+index = 1
 while (not done):
     for j in range(0, len(currentNode)):
         for i in range(0, currLen):
             childNode = currentNode[i]
-            print(childNode.data)
             childNode.addNode1(childernFinder(childNode.data, dictonary))
+            for i in range(0, len(childNode.children1)):
+                fringe.put(childNode.children1[i].data)
+                
             if(childNode.data == GoalState):
-                ##path finder function call
                 temp = treeRoot.root
-                print(findPath(startState, "try", temp))
+                print("From:", startState, "to:", GoalState)
+                print("result:", findPath(startState, GoalState, temp))
+                print("fringe:", list(fringe.queue))
                 done = True
-                sys.exit()
+                break
+            else:
+                fringe.get()
+        if done:
+            break
+        if(j >= len(currentNode)):
+            currentNode = treeRoot.root.children1[index].children1
+            index += 1
+            continue
+        
         currentNode = currentNode[j].children1
         currLen = len(currentNode)
-    
+        
+    index -= 1
+    currentNode = treeRoot.root.children1[index].children1
+    index += 1
 
-
-
-
-
-
-"""while (not done):
-    for i in range(0, currLen):
-        childNode = currentNode.children1[i]
-        print(childNode.data)
-        childNode.addNode1(childernFinder(childNode.data, dictonary))
-        if(childNode.data == GoalState):
-            ##path finder function call
-            done = True
-            break
-    currentNode = currentNode.children1
-    currLen = len(currentNode)
-"""
 
 
 
