05856759758b3969f41952977652d2fdd34ffd27
unknown <cristiano_aguilar19@hotmail.com>
2020-09-30 00:20:59 -0700

    lab5 finished the functions just need to add the options to chose from manhattan or eucildean method

--- lab5.py
+++ lab5.py
@@ -5,6 +5,7 @@ Created on Tue Sep 29 11:11:54 2020
 @author: Cristian Aguilar
 """
 import math
+import numpy as np
 class Node():
     def __init__(self, data,cost,x,y):
         self.data = data
@@ -15,31 +16,32 @@ class Node():
         self.y = y
 
 def outOfboundsCheck(x, y):
-    if(x <=-1 or y <= -1 or x >= 6 or y >= 6):
+    if(x <=-1 or y <= -1 or x >= 5 or y >= 7):
         return True
     else:
         return False
 
 def eucildean_distance(currX, currY, goalX, goalY):
-    x = abs(currX - goalX) - 1
-    print(x)
-    y = abs(currY - goalY) - 1
-    print(y)
+    x = abs(currX - goalX)
+    y = abs(currY - goalY)
     z = x**2 + y**2
     z = math.sqrt(z)
     return z
 
 def manhattan_distance(currX, currY, goalX, goalY):
-    x = abs(currX - goalX)-1
-    y = abs(currY - goalY)-1
+    x = abs(currX - goalX)
+    y = abs(currY - goalY)
     return x + y
 
 def nextInTheFringe(fringeList):
     closestIndex = 0
     closest = fringeList[closestIndex]
-    for index in range(0, len(fringeList)):
+    for index in range(0, len(fringeList), 1):
         if(fringeList[index].cost < closest.cost):
             closestIndex = index
+            closest = fringeList[closestIndex]
+    
+
     return fringe.pop(closestIndex)
 
 def successor_fcn(currNode, fringeList, visitedList):
@@ -50,21 +52,21 @@ def successor_fcn(currNode, fringeList, visitedList):
     #left
     if(not outOfboundsCheck(x, y+1) and graph[x][y+1] != 'x'):
         if((x,y+1) not in visitedList):
-            child = Node(graph[x][y+1], manhattan_distance(x, y+1, 6, 6),x,y+1)
+            child = Node(graph[x][y+1], manhattan_distance(x, y+1, 4, 6),x,y+1)
             child.parent = currNode
             currNode.children.append(child)
             fringeList.append(child)
     #right
     if(not outOfboundsCheck(x, y-1) and graph[x][y-1] != 'x'):
         if((x,y-1) not in visitedList):
-            child = Node(graph[x][y-1], manhattan_distance(x, y-1, 6, 6),x,y-1)
+            child = Node(graph[x][y-1], manhattan_distance(x, y-1, 4, 6),x,y-1)
             child.parent = currNode
             currNode.children.append(child)
             fringeList.append(child)
     #up
     if(not outOfboundsCheck(x-1, y) and graph[x-1][y] != 'x'):
         if((x-1,y) not in visitedList):
-            child = Node(graph[x-1][y], manhattan_distance(x-1, y, 6, 6),x-1,y)
+            child = Node(graph[x-1][y], manhattan_distance(x-1, y, 4, 6),x-1,y)
             child.parent = currNode
             currNode.children.append(child)
             fringeList.append(child)
@@ -72,30 +74,48 @@ def successor_fcn(currNode, fringeList, visitedList):
     #down
     if(not outOfboundsCheck(x+1, y) and graph[x+1][y] != 'x'):
         if((x+1,y) not in visitedList):
-            child = Node(graph[x+1][y], manhattan_distance(x+1, y, 6, 6),x+1,y)
+            child = Node(graph[x+1][y], manhattan_distance(x+1, y, 4, 6),x+1,y)
             child.parent = currNode
             currNode.children.append(child)
             fringeList.append(child)
             
-#def greedy(currentNode):
+def greedy(currentNode):
+    fringe.append(currentNode)
+    nextNode = nextInTheFringe(fringe)
+    graphPrint[nextNode.x][nextNode.y] = '*'
+    print(np.matrix(graphPrint))
+    successor_fcn(nextNode,fringe,visited)
+    #check goal
+    if(nextNode.data == 'G'):
+            return nextNode
     
-graph = [['S',2,3,1,1,'x',3],
+    while fringe != []:
+        nextNode = nextInTheFringe(fringe)
+        graphPrint[nextNode.x][nextNode.y] = '*'
+        print(np.matrix(graphPrint), "\n")
+        #check goal
+        if(nextNode.data == 'G'):
+            return nextNode
+        successor_fcn(nextNode,fringe, visited)
+    
+graph = [['S',2,3,'x',1,'x',3],
          [1,1,'x','x',3,'x',1],
          [3,1,'x',1,1,1,1],
          [1,1,3,4,4,1,2],
          [1,4,2,1,'x',1,'G']]
 
+graphPrint = [['-','-','-','-','-','-','-'],
+              ['-','-','-','-','-','-','-'],
+              ['-','-','-','-','-','-','-'],
+              ['-','-','-','-','-','-','-'],
+              ['-','-','-','-','-','-','-']]
 fringe = []
 visited = []
-root = Node('S', 0, 3,4)
+root = Node('S', 0, 0,0)
 
+endNode = greedy(root)
+print(visited)
 
-
-print(eucildean_distance(0,0, 6,6))
-
-print(manhattan_distance(0,0,6,6))
-successor_fcn(root, fringe, visited)
-nextNode = nextInTheFringe(fringe)
 
 
 
